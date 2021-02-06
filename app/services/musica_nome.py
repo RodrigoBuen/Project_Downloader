b7494c7e6f6a62286acb938e1452374cf2c53c1f
RodrigoBuen <rodrigo.troll@devwebsystem.com.br>
2021-02-06 11:12:11 -0300

    Updates modulos e serviços

---
+++ app/services/musica_nome.py
@@ -0,0 +1,39 @@
+from googlesearch import search
+import youtube_dl
+
+campo_entry = ''
+
+class Download():
+    def __init__(self):
+        self.pesquisa()
+        self.run()
+
+    def pesquisa(self):
+        self.links = []
+        for self.musica in search(campo_entry +' lyrics youtube', stop=1):
+            if 'https://www.youtube.com/watch' in self.musica:
+                self.links.append(self.musica)
+                print(self.musica)
+
+    def run(self):
+        self.video_url = self.links[0]
+        self.video_info = youtube_dl.YoutubeDL().extract_info(
+            url=self.video_url, download=False)
+
+        self.filename = f"{self.video_info['title']}.mp3"
+
+        self.options = {
+            'format': 'bestaudio / best',
+            'keepvideo': False,
+            'outtmpl': self.filename,
+            'postprocessors': [{'key': 'FFmpegExtractAudio',
+                                'preferredcodec': 'mp3',
+                                'preferredquality': '256', }]
+        }
+
+        try:
+            with youtube_dl.YoutubeDL(self.options) as ydl:
+                ydl.download([self.video_info['webpage_url']])
+
+        except:
+            print('Baixada com sucesso')
--- app/views/control_view.py
+++ app/views/control_view.py
@@ -1,7 +1,21 @@
-from flask import render_template
-
+from flask import render_template, request, redirect, url_for
 from app import app
+from app.services import musica_nome
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/musica-nome", methods=['GET','POST'])
+def musica_nome():
+    if request.method == 'POST':
+        texto = request.form['texto']
+        musica_nome.campo_entry = texto
+        musica_nome.Download()
+        
+        return redirect(url_for('home'))
+    return render_template("musica_nome.html")
+
+@app.route("/musica-link")
+def musica_link():
+    return render_template("musica_link.html")
