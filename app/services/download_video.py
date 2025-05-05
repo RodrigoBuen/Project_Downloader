bd42612418787c3793d3ac421e9083836513b3cf
RodrigoBuen <rbartecriativa@gmail.com>
2025-05-05 14:50:12 -0300

    Atualizado video_download

--- app/services/download_video.py
+++ app/services/download_video.py
@@ -1,30 +1,34 @@
-from pytube import YouTube, Playlist
+from pytubefix import YouTube, Playlist
+from pytubefix.cli import on_progress
 
 url = ''
 
 class Download_Video():
     def __init__(self):
-        self.tratativa()
+        self.escolha()
 
     def download_video(self):
-        yt = YouTube(url)
-        video = yt.streams.get_highest_resolution()
-        print(video)
-        video.download()
+        yt = YouTube(url, on_progress_callback=on_progress)
+        print(yt.title)
+        
+        ys = yt.streams.get_highest_resolution()
+        ys.download()
+        
 
     def download_playlist(self):
         playlist = Playlist(url)
-        for item in playlist:
-            yt = YouTube(item)
-            video = yt.streams.get_highest_resolution()
-            print(video)
-            video.download(output_path='playlist')
+        for video in playlist.videos:
+            print(video.title)
+            
+            ys = video.streams.get_highest_resolution()
+            ys.download()
+            print('\n Completo!!!')
 
-    def tratativa(self):
-        if 'https://www.youtube.com/watch' in url:
+    def escolha(self):
+        if 'www.youtube.com/watch' in url and not 'list=' in url:
             self.download_video()
             print('Download Completo')
 
-        elif 'https://www.youtube.com/playlist' in url:
+        elif 'www.youtube.com/watch' in url and 'list=' in url:
             self.download_playlist()
             print('Download Completo')
