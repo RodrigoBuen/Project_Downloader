5c82cf18ae9c7cac490308cd1acecdd167c010a3
RodrigoBuen <rbartecriativa@gmail.com>
2025-05-06 18:23:01 -0300

    atulizado serviços

--- app/services/download_nome.py
+++ app/services/download_nome.py
@@ -1,6 +1,13 @@
 from googlesearch import search
-import youtube_dl
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+from moviepy import *
 from time import sleep
+import os, shutil
+from pathlib import Path
+
+# Definindo o caminho raiz
+pasta_atual = Path(".").resolve()
 
 campo_entry = ''
 
@@ -8,7 +15,7 @@ class Download_nome():
     def __init__(self):
         self.pesquisa()
         self.run()
-        sleep(1)
+        self.move_create()
 
     def pesquisa(self):
         self.links = []
@@ -19,23 +26,35 @@ class Download_nome():
 
     def run(self):
         self.video_url = self.links[0]
-        self.video_info = youtube_dl.YoutubeDL().extract_info(
-            url=self.video_url, download=False)
-
-        self.filename = f"{self.video_info['title']}.mp3"
-
-        self.options = {
-            'format': 'bestaudio / best',
-            'keepvideo': False,
-            'outtmpl': self.filename,
-            'postprocessors': [{'key': 'FFmpegExtractAudio',
-                                'preferredcodec': 'mp3',
-                                'preferredquality': '256', }]
-        }
+        self.url_extractor = YouTube(self.video_url, on_progress_callback=on_progress)
+        print(self.url_extractor.title)
 
         try:
-            with youtube_dl.YoutubeDL(self.options) as ydl:
-                ydl.download([self.video_info['webpage_url']])
+            self.ys = self.url_extractor.streams.get_audio_only()
+            self.ys.download()
+            sleep(2)
+            
+            self.audio_a4 = AudioFileClip(f'{self.url_extractor.title}.m4a')
+            print('\n arquivo encontrado')
+            
+            self.audio_a4.write_audiofile(f'{self.url_extractor.title}.mp3')
+            print('\n convertido com sucesso')
+            
+            os.remove(f'{pasta_atual}/{self.url_extractor.title}.m4a')
+            print('\n deletado com sucesso')
+            
 
         except:
             print('Baixada com sucesso')
+            
+    def move_create(self):
+        if os.path.isdir('Musicas'):
+            shutil.move(f'{pasta_atual}/{self.url_extractor.title}.mp3', f'{pasta_atual}/Musicas/{self.url_extractor.title}.mp3')
+            print('Movido com sucesso')
+            
+        else:
+            os.mkdir('Musicas')
+            sleep(1)
+            print('Pasta criada com sucesso')
+            shutil.move(f'{pasta_atual}/{self.url_extractor.title}.mp3', f'{pasta_atual}/Musicas/{self.url_extractor.title}.mp3')
+            print('Movido com sucesso')
--- app/services/download_video.py
+++ app/services/download_video.py
@@ -1,5 +1,9 @@
 from pytubefix import YouTube, Playlist
 from pytubefix.cli import on_progress
+import os, shutil, time
+from pathlib import Path
+
+pasta_atual = Path(".").resolve()
 
 url = ''
 
@@ -8,27 +12,50 @@ class Download_Video():
         self.escolha()
 
     def download_video(self):
-        yt = YouTube(url, on_progress_callback=on_progress)
-        print(yt.title)
+        self.yt = YouTube(url, on_progress_callback=on_progress)
+        print(self.yt.title)
         
-        ys = yt.streams.get_highest_resolution()
-        ys.download()
+        self.ys = self.yt.streams.get_highest_resolution()
+        self.ys.download()
+        
+        if os.path.isdir(f'{pasta_atual}/Videos'):
+                shutil.move(f'{pasta_atual}/{self.yt.title}.mp4', f'{pasta_atual}/Videos/{self.yt.title}.mp4')
+                print('\n Movido com sucesso')
+                
+        else:
+            os.mkdir(f'{pasta_atual}/Videos')
+            time.sleep(1)
+            print('Pasta criada com sucesso')
+            shutil.move(f'{pasta_atual}/{self.yt.title}.mp4', f'{pasta_atual}/Videos/{self.yt.title}.mp4')
+            print('\n Movido com sucesso')
+
         
 
     def download_playlist(self):
-        playlist = Playlist(url)
-        for video in playlist.videos:
-            print(video.title)
+        self.playlist = Playlist(url)
+        for self.video in self.playlist.videos:
+            print(self.video.title)
             
-            ys = video.streams.get_highest_resolution()
+            ys = self.video.streams.get_highest_resolution()
             ys.download()
             print('\n Completo!!!')
+            
+            if os.path.isdir('Videos'):
+                shutil.move(f'{pasta_atual}/{self.video.title}.mp4', f'{pasta_atual}/Videos/{self.video.title}.mp4')
+                print('Movido com sucesso')
+                
+            else:
+                os.mkdir('Videos')
+                time.sleep(1)
+                print('Pasta criada com sucesso')
+                shutil.move(f'{pasta_atual}/{self.video.title}.mp4', f'{pasta_atual}/Videos/{self.video.title}.mp4')
+                print('Movido com sucesso')
 
     def escolha(self):
         if 'www.youtube.com/watch' in url and not 'list=' in url:
             self.download_video()
             print('Download Completo')
-
+        
         elif 'www.youtube.com/watch' in url and 'list=' in url:
             self.download_playlist()
             print('Download Completo')
